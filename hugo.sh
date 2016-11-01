82c2679c54844c665a3ff5c23dd71ba72f66013a
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-11-01 10:04:24 +0300

    Сделал относительные ссылки на скрипты и стили

--- hugo.sh
+++ hugo.sh
@@ -4,11 +4,11 @@ set +e
 HUGO=`which hugo`
 set -e
 if [ "${HUGO}" == "" ]; then
-  HUGO_VER=0.16
+  HUGO_VER=0.17
   if [ ! -f .build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64 ]; then
     mkdir -p .build
     tar -xzvf .jenkins/distrib/hugo_${HUGO_VER}_linux-64bit.tgz -C .build
   fi
-  HUGO=".build/hugo"
+  HUGO=".build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64"
 fi
 ${HUGO} $@
