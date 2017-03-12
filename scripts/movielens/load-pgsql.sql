1c1e3406037665121e1cc054547287248a8e2fb8
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-03-12 10:00:36 +0300

    Вводная к индексам

--- scripts/movielens/load-pgsql.sql
+++ scripts/movielens/load-pgsql.sql
@@ -4,6 +4,7 @@ CREATE EXTENSION IF NOT EXISTS citext;
 create table movies (
   id serial primary key,
   title citext not null,
+  year int,
   genres varchar(1024)
 );
 
@@ -93,6 +94,9 @@ select m.id, g.id from (
   where genres <> '(no genres listed)'
 ) m
 join genres g on (m.genre = g.name);
+
+update movies set year = cast(substring(title from '\(((20|19|18)\d{2})\)\s*$') as int);
+update movies set title = substring(title from '^(.*?)\s*\((20|19|18)\d{2}\)\s*$') where year is not null;
 
 alter table movies
   drop column genres;
