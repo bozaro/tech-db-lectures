c065f1eeba92073bd85a4bda22bfccb4df72ac16
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-03-12 22:50:55 +0300

    Актуализация слайдов

--- scripts/movielens/load-pgsql.sh
+++ scripts/movielens/load-pgsql.sh
@@ -3,6 +3,6 @@ DB=movielens
 set -e -x
 dropdb --if-exists $DB
 wget -c http://files.grouplens.org/datasets/movielens/ml-latest.zip
-unzip ml-latest.zip
+unzip -o ml-latest.zip
 createdb $DB
 psql $DB < load-pgsql.sql
