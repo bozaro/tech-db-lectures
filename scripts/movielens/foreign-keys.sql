d4b791c755b1c403b14ba0bea54661d081d16657
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-03-24 23:25:00 +0300

    Индексы

--- scripts/movielens/foreign-keys.sql
+++ scripts/movielens/foreign-keys.sql
@@ -1,1 +1,8 @@
-foreign-keys.sql
+create index idx_links_movie_id on links (movie_id);
+create index idx_movie_genres_movie_id on movie_genres (movie_id);
+create index idx_movie_genres_genre_id on movie_genres (genre_id);
+
+create index idx_movie_tags_movie_id on movie_tags (movie_id);
+create index idx_movie_tags_tag_id on movie_tags (tag_id);
+
+create index idx_ratings_movie_id on ratings (movie_id);
