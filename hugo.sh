c3694164e3b415f07510413b1cbac2f64a3862cc
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-09-11 23:18:13 +0300

    2-ая лекция

--- hugo.sh
+++ hugo.sh
@@ -1,15 +1,31 @@
 #!/bin/bash -ex
 cd `dirname $0`
-set +e
-HUGO=`which hugo`
-set -e
-if [ "${HUGO}" == "" ]; then
-  HUGO_VER=0.17
-  if [ ! -f .build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64 ]; then
-    mkdir -p .build
-    tar -xzvf .jenkins/distrib/hugo_${HUGO_VER}_linux-64bit.tgz -C .build
-  fi
-  HUGO=".build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64"
+HUGO_VER=0.27
+OS=`uname`
+case "${OS}" in
+	Linux)
+		HUGO_DIST="hugo_${HUGO_VER}_Linux-64bit.tar.gz"
+		;;
+	Darwin)
+		HUGO_DIST="hugo_${HUGO_VER}_macOS-64bit.tar.gz"
+		;;
+	*)
+		echo "Unknown OS: ${OS}"
+		exit 1
+		;;
+esac
+
+HUGO_DIR=".build/${OS}_${HUGO_VER}"
+HUGO="${HUGO_DIR}/hugo"
+if [ ! -f "${HUGO}" ]; then
+	mkdir -p "${HUGO_DIR}"
+	mkdir -p ".jenkins"
+	if [ ! -f ".jenkins/${HUGO_DIST}" ]; then
+		wget -v -c -O ".jenkins/${HUGO_DIST}~" "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VER}/${HUGO_DIST}"
+		mv ".jenkins/${HUGO_DIST}~" ".jenkins/${HUGO_DIST}"
+	fi
+	tar -xzvf ".jenkins/${HUGO_DIST}" -C "${HUGO_DIR}/"
 fi
-${HUGO} $@
+
+"${HUGO}" $@
 find public/ -iname ".*" -exec rm {} \;
